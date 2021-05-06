99f248bc3c2d975356297a32a32f2c5ee7a3faa9
Olavo Mello <olavo@agenciadix.com.br>
2021-05-05 23:52:06 -0300

    Add new functions

--- helpers.js
+++ helpers.js
@@ -2,6 +2,9 @@
     Functions and helpers
 */
 
+var fs = require('fs');
+var path = require('path');
+
 // Container for helpers
 const helpers = {}; 
 
@@ -53,6 +56,55 @@ helpers.controllerMethods = function( req, res, acceptableMethods = ['GET','POST
         return false;
     }    
 }
+
+// Generate unique id
+helpers.uuid = function() {
+    // Token size
+    const COUNT = 20;
+    // String path
+    var _sym = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    // Return
+    var str  = '';
+    // Generate string
+    for(var i = 0; i < COUNT; i++)
+        str += _sym[parseInt(Math.random() * (_sym.length))];
+
+    return str;
+}
+
+// create file
+helpers.fileAdd = function( file, data, path, callback ){
+    if( !file && !data && !path ) {
+        // Error
+        callback("Error manipulating file. Parameters require empty.");
+    } else {
+        // Ok
+        const dir = path.join(__dirname,"/../." + path);
+
+        if( !path.existsSync(dir) ){
+            // Path dont exist. Try to create 
+            try{
+                // Path created
+                fs.mkdirSync(dir);
+            } catch( err ){
+                // Error creating path
+                callback("Error creating path " + dir );
+            }
+
+            // Create file
+            fs.writeFile( dir +"/"+ file, data, function(err) {
+                if(err) {
+                    // Error writing file
+                    callback("Error creating file " + err );
+                } else {
+                    // File writed
+                    callback(false);
+                }
+            }); 
+        }
+    }
+}
+
 
 // Export
 module.exports = helpers;
