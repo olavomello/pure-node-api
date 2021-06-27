c8095cb654a1e64cc29d27a7a7acae7bc4d21d92
Olavo Mello <olavo@agenciadix.com.br>
2021-06-26 23:34:03 -0300

    Allow user loggedout view mmenu

--- controllers/api/product.js
+++ controllers/api/product.js
@@ -45,42 +45,7 @@ controller.menu = async ( req, res, arrPath ) => {
     //
     if(  controllerMethods( req, res, ["GET"] ) ){
       // Execute payload return
-  
-      // User token
-      var token = String(req.headers.token).trim();
-      // Token validation
-      token     = ( token && token != "undefined" ? token : false );
-  
-      if( token ){
-        // Token exist
-        if( userLogged = fileRead(token,"tokens") ){
-    
-          // Check token expiration
-          if( userLogged.expire < Date.now()) {
-            // Token expired
-            res.writeHead( 404 ).end( JSON.stringify( { error : false, message : "Token expired. Please do login again."} ) );            
-          } else {
-            // Token ok and User exist
-        
-            // Update user token
-            tokenUpdate(req);
-                        
-            res.end( JSON.stringify(_PRODUCTS));          
-          }
-        } else {
-          // User does not exist
-          res.writeHead( 200 ).end( JSON.stringify( { error : false, message : "User not found"} ) );        
-        }
-      } else {
-        // Token does not passed
-        res.writeHead( 404 );
-        // Return // payload
-        const payload = {
-          error   : true,
-          message : "User token empty"
-        }
-        res.end( JSON.stringify( payload ) );
-      } 
+      res.end( JSON.stringify(_PRODUCTS));
     }
   };
 
